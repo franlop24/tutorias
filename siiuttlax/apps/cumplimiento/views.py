0f010d03bb89b1dcf4be44694578364ad05eb51e
Miguel Angel Islas Mendez <islasmiguel737@gmail.com>
2024-08-14 11:13:17 -0600

    etc

--- siiuttlax/apps/cumplimiento/views.py
+++ siiuttlax/apps/cumplimiento/views.py
@@ -22,7 +22,6 @@ def consultas_por_periodo(request, periodo_id):
     period = get_object_or_404(Period, id=periodo_id)
     grupos = Group.objects.filter(period=period)
     
-    # Determinar los meses incluidos en el periodo seleccionado
     period_months = {
         'Enero - Abril': [1, 2, 3, 4],
         'Mayo - Agosto': [5, 6, 7, 8],
@@ -30,7 +29,6 @@ def consultas_por_periodo(request, periodo_id):
     }
     months_available = period_months.get(period.period, [])
     
-    # Obtener el mes del parámetro GET, si existe
     mes = request.GET.get('mes', None)
     
     grupos_data = []
@@ -39,7 +37,6 @@ def consultas_por_periodo(request, periodo_id):
     for grupo in grupos:
         actividades = ReporteTutoria.objects.filter(grupo=grupo)
         
-        # Filtrar reportes por mes solo si se selecciona un mes válido
         if mes and int(mes) in months_available:
             actividades = actividades.filter(fecha_tutoria__month=int(mes))
         
@@ -52,10 +49,8 @@ def consultas_por_periodo(request, periodo_id):
                 'evidencia_canalizacion_alumno': actividad.evidencia_canalizacion_alumno.url if actividad.evidencia_canalizacion_alumno else None
             })
         
-        # Completar con actividades "No reportado" si faltan
         actividades_completas = actividades_data + [{'nombre_actividad': 'No reportado', 'evidencia_lista_asistencia': None, 'evidencia_canalizacion_alumno': None}] * (num_actividades_esperadas - len(actividades_data))
         
-        # Mostrar el grupo si tiene actividades o si no se ha seleccionado un mes
         if not mes or actividades_data:
             grupos_data.append({
                 'Cuatrimestre': grupo.semester.semester_name,
