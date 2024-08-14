5fec1dbf502669d0e61dfe093e5ee8e8655f6c44
Miguel Angel Islas Mendez <islasmiguel737@gmail.com>
2024-08-14 15:45:22 -0600

    channeling and monitoring

--- siiuttlax/apps/cumplimiento/views.py
+++ siiuttlax/apps/cumplimiento/views.py
@@ -21,14 +21,13 @@ def index(request):
 def consultas_por_periodo(request, periodo_id):
     period = get_object_or_404(Period, id=periodo_id)
     grupos = Group.objects.filter(period=period)
-    
     period_months = {
         'Enero - Abril': [1, 2, 3, 4],
         'Mayo - Agosto': [5, 6, 7, 8],
         'Septiembre - Diciembre': [9, 10, 11, 12]
     }
     months_available = period_months.get(period.period, [])
-    
+
     mes = request.GET.get('mes', None)
     
     grupos_data = []
@@ -36,7 +35,7 @@ def consultas_por_periodo(request, periodo_id):
     
     for grupo in grupos:
         actividades = ReporteTutoria.objects.filter(grupo=grupo)
-        
+
         if mes and int(mes) in months_available:
             actividades = actividades.filter(fecha_tutoria__month=int(mes))
         
@@ -51,6 +50,8 @@ def consultas_por_periodo(request, periodo_id):
         
         actividades_completas = actividades_data + [{'nombre_actividad': 'No reportado', 'evidencia_lista_asistencia': None, 'evidencia_canalizacion_alumno': None}] * (num_actividades_esperadas - len(actividades_data))
         
+        actividades_completas = actividades_data + [{'nombre_actividad': 'No reportado', 'evidencia_lista_asistencia': None, 'evidencia_canalizacion_alumno': None}] * (num_actividades_esperadas - len(actividades_data))
+
         if not mes or actividades_data:
             grupos_data.append({
                 'Cuatrimestre': grupo.semester.semester_name,
